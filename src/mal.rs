ce0c9f3c669b5de67a09a6a02b090092da4c323d
Tim <tjf20@hotmail.co.uk>
2024-12-26 11:32:20 +0000

    Reads all files in the data directory

--- src/mal.rs
+++ src/mal.rs
@@ -1,7 +1,6 @@
-use std::io::prelude::*;
 use crate::anime_manga::AnimeManga;
 
-use std::fs::File;
+use std::fs::{File, read_dir};
 use std::io::BufReader;
 use xml::reader::{EventReader, XmlEvent};
 
@@ -17,68 +16,74 @@ impl Data {
     }
 
     pub fn search(&mut self) {
-        self.xml_read();
+        let _ = self.xml_read();
     }
 
     pub fn xml_read(&mut self) -> std::io::Result<()> {
-        // I want this to read everything in the data folder
-        let file = File::open("data/animelist_1734298612_-_16433979.xml")?;
-        let file = BufReader::new(file); // Buffering is important for performance
-        let parser = EventReader::new(file);
-        let mut format = String::new();
-        let mut title = String::new();
-        let mut score = String::new();
-        let mut status = String::new();
-        let mut next_field = String::new();
+        let files = read_dir("./data")?;
+        for file in files {
+            let file = file?;
+            let file_path = file.path();
 
-        // Possible matches are listed here: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
-        for element in parser {
-            match element {
-                Ok(XmlEvent::StartElement { name, .. }) => {
-                    if name.local_name == "anime" {
-                        format = String::from("anime");
-                    }
-                    else if name.local_name == "manga" {
-                        format = String::from("manga");
-                    }
-                    else if name.local_name == "series_title" {
-                        next_field = String::from("series_title");
-                    }
-                    else if name.local_name == "my_score" {
-                        next_field = String::from("my_score");
-                    }
-                    else if name.local_name == "my_status" {
-                        next_field = String::from("my_status");
-                    }
-                },
-                Ok(XmlEvent::CData(s)) | Ok(XmlEvent::Characters(s)) => {
-                    if next_field == "series_title" {
-                        title = s;
-                        next_field = String::new();
-                    }
-                    else if next_field == "my_score" {
-                        score = s;
-                        next_field = String::new();
-                    }
-                    else if next_field == "my_status" {
-                        status = s;
-                        next_field = String::new();
-                    }
-                },
-                Err(e) => println!("Error: {:?}", e),
-                _ => {},
-            }
+            let file_open = File::open(file_path)?;
+            let file_open = BufReader::new(file_open); // Buffering is important for performance
+            let parser = EventReader::new(file_open);
 
-            if (format != "" && title != "" && score != "" && status != "") {
-                self.items.push(AnimeManga {
-                    format: format.clone(),
-                    rating: score.clone(),
-                    status: status.clone(),
-                    name:   title.clone(),
-                });
-                title = String::new();
-                score = String::new();
-                status = String::new();
+            let mut format = String::new();
+            let mut title = String::new();
+            let mut score = String::new();
+            let mut status = String::new();
+            let mut next_field = String::new();
+
+            // Possible matches are listed here: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
+            for element in parser {
+                match element {
+                    Ok(XmlEvent::StartElement { name, .. }) => {
+                        if name.local_name == "anime" {
+                            format = String::from("anime");
+                        }
+                        else if name.local_name == "manga" {
+                            format = String::from("manga");
+                        }
+                        else if name.local_name == "series_title" || name.local_name == "manga_title" {
+                            next_field = String::from("series_title");
+                        }
+                        else if name.local_name == "my_score" {
+                            next_field = String::from("my_score");
+                        }
+                        else if name.local_name == "my_status" {
+                            next_field = String::from("my_status");
+                        }
+                    },
+                    Ok(XmlEvent::CData(s)) | Ok(XmlEvent::Characters(s)) => {
+                        if next_field == "series_title" {
+                            title = s;
+                            next_field = String::new();
+                        }
+                        else if next_field == "my_score" {
+                            score = s;
+                            next_field = String::new();
+                        }
+                        else if next_field == "my_status" {
+                            status = s;
+                            next_field = String::new();
+                        }
+                    },
+                    Err(e) => println!("Error: {:?}", e),
+                    _ => {},
+                }
+
+                if format != "" && title != "" && score != "" && status != "" {
+                    self.items.push(AnimeManga {
+                        format: format.clone(),
+                        rating: score.clone(),
+                        status: status.clone(),
+                        name:   title.clone(),
+                    });
+                    title = String::new();
+                    score = String::new();
+                    status = String::new();
+                }
             }
         }
 
